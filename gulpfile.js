6212bcc5b9568b21932b7c402d93a18a4030d78f
dmitriy <dmitriy1a22222@gmail.com>
2018-10-19 00:19:54 +0300

    added new html page

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ var gulp       = require('gulp'), // Подключаем Gulp
 // var gulpUtil = require('gulp-util');
 
 gulp.task('sass', function(){ // Создаем таск Sass
-	return gulp.src('app/sass/**/*.sass') // Берем источник
+	return gulp.src(['app/sass/**/*.sass', 'app/sidePage/mainMenuPage/sass/**/*.sass']) // Берем источник
 		.pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
 		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
 		.pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
@@ -52,8 +52,8 @@ gulp.task('css', ['sass'], function() {
 });
 
 gulp.task('watch', ['browser-sync', 'css', 'scripts'], function() {
-	gulp.watch('app/sass/**/*.sass', ['sass']); // Наблюдение за sass файлами в папке sass
-	gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
+	gulp.watch(['app/sass/**/*.sass', 'app/sidePage/mainMenuPage/sass/**/*.sass'], ['sass']); // Наблюдение за sass файлами в папке sass
+	gulp.watch(['app/*.html', 'app/sidePage/mainMenuPage/*.html', 'app/sidePage/infoPage/*.html'], browserSync.reload); // Наблюдение за HTML файлами в корне проекта
 	gulp.watch('app/js/**/*.js', browserSync.reload);   // Наблюдение за JS файлами в папке js
 });
 
